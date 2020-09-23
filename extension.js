e9677ee0cb51c80b9c593bc32bc60a922100defa
Renan Gueiros <renangueiros@outlook.com>
2020-09-22 22:50:05 -0300

    Add settings schema

--- extension.js
+++ extension.js
@@ -1,5 +1,14 @@
+const Gio = imports.gi.Gio;
+
+const AppDisplay = imports.ui.appDisplay;
 const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
 const Me = ExtensionUtils.getCurrentExtension();
+
+const EXTENSION_SCHEMA = 'org.gnome.shell.extensions.app-grid-columns';
+const COLUMNS_KEY = 'columns';
+
+const GNOME_DEFAULT_COLUMNS = 6;
 
 class Extension {
 
@@ -9,10 +18,45 @@ class Extension {
 
 	enable() {
 		log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+
+		let gschema = Gio.SettingsSchemaSource.new_from_directory(
+			Me.dir.get_child('schemas').get_path(),
+			Gio.SettingsSchemaSource.get_default(),
+			false,
+		);
+
+		this.settings = new Gio.Settings({
+			settings_schema: gschema.lookup(
+				EXTENSION_SCHEMA,
+				true,
+			),
+		});
+
+		this.columns = this.settings.get_int(COLUMNS_KEY);
+
+		this._onColumnsChangedId = this.settings.connect(
+			`changed::${COLUMNS_KEY}`,
+			this._onColumnsChanged,
+		);
+
+		this._onColumnsChanged();
 	}
 
 	disable() {
 		log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+
+		this.settings.disconnect(this._onColumnsChangedId);
+
+		this._udpateColumns(GNOME_DEFAULT_COLUMNS);
+	}
+
+	_onColumnsChanged() {
+		this.columns = this.settings.get_int(COLUMNS_KEY);
+		this._udpateColumns(this.columns);
+	}
+
+	_udpateColumns(columns) {
+		log(`Displaying ${columns} columns in the App Grid}`);
 	}
 }
 
